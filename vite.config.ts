59b9607b9158d7c03000b772acb210bd793c0370
Jayashree K S <jayashree.ks@tsgforce.com>
2025-08-28 17:27:07 +0530

    Fixed issues for render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://fuelboss-server.onrender.com",
         changeOrigin: true,
         secure: false,
       },
